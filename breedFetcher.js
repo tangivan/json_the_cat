a382426b358eb9fb8b9713ca1fd29cd565ac8dc2
Ivan <ivanxintang@gmail.com>
2021-11-04 13:18:17 -0700

    Add error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,10 +5,12 @@ args = args.slice(2);
 
 let url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
 request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
+  if (error) {
+    console.log(error.code);
+  } else if (body.length === 2) {
     console.log('Breed was not found');
   } else {
+    const data = JSON.parse(body);
     console.log(data[0].description);
   }
 });
